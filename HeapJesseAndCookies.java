a2a571c500b3a7c9c78b6d643352976e2d66d93b
Nil Seri <senoritadeveloper01@gmail.com>
2021-11-26 12:30:47 +0300

    Create HeapJesseAndCookies.java

---
+++ HeapJesseAndCookies.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+// https://www.hackerrank.com/challenges/jesse-and-cookies/problem
+// IMPORTANT! Select Java 15 (Java 8 gives timeout error)
+
+class Result {
+
+    /*
+     * Complete the 'cookies' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     */
+
+    public static int cookies(int k, List<Integer> A) {
+    // Write your code here
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        A.stream().forEach(a -> minHeap.add(a));
+        boolean isOverThreshold = minHeap.peek() >= k;
+        Integer iterationCount = 0;
+        while (!isOverThreshold && minHeap.size() > 1) {
+            minHeap.add(minHeap.poll() + 2 * minHeap.poll());
+            isOverThreshold = minHeap.peek() >= k;
+            iterationCount ++;
+        }
+        return isOverThreshold ? iterationCount : -1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
